573762f047e759f014192477dd3146d695907f6b
ivan <icontador@gmail.com>
2025-10-29 09:25:59 +0100

    estructura backend hecha

---
+++ backend/src/test/java/com/agencia/backend/AgenciaViajesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agencia.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenciaViajesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
